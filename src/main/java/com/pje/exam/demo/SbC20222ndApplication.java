2d77fb18b7a81439de9f846d70c3f402e52637d8
fmfu51 <fmfu5129@gmail.com>
2022-02-16 13:08:09 +0900

    마이바티스 및 쿼리에 메서든 여결

---
+++ src/main/java/com/pje/exam/demo/SbC20222ndApplication.java
@@ -0,0 +1,13 @@
+package com.pje.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbC20222ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbC20222ndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pje/exam/demo/SbC20222ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pje.exam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbC20222ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
